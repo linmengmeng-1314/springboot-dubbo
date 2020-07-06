816ea614779f2dd92abbd06daf689576f726ca0d
EDZ <EDZ@WIN-HQNMB7O8EQP>
2020-07-06 15:11:09 +0800

    modify loadbalance value to leastactive

--- springboot-dubbo-provider/src/main/java/com/lin/springboot/dubbo/service/user/provider/api/impl/UserServiceImpl.java
+++ springboot-dubbo-provider/src/main/java/com/lin/springboot/dubbo/service/user/provider/api/impl/UserServiceImpl.java
@@ -1,12 +1,23 @@
 package com.lin.springboot.dubbo.service.user.provider.api.impl;
-    
-    import com.alibaba.dubbo.config.annotation.Service;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import com.alibaba.dubbo.config.annotation.Service;
 import com.lin.springboot.dubbo.service.user.api.UserService;
-    
-    @Service(version = "${user.service.version}")
-    public class UserServiceImpl implements UserService {
-        @Override
-        public String sayHi() {
-            return "Hello Dubbo";
-        }
-    }
+
+/***
+ * 通过 @Service 注解实现服务提供方
+ * @author linmengmeng
+ * @date 2020年7月6日下午3:00:16
+ */
+@Service(version = "${user.service.version}")
+public class UserServiceImpl implements UserService {
+
+	@Value("${dubbo.protocol.port}")
+	private String port;
+
+	@Override
+	public String sayHi() {
+		return "Hello Dubbo , i am from port:" + port;
+	}
+}
